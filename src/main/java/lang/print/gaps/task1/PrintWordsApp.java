853ad5482fbf5b6825ab9723815952da7c554126
Shohabbos Usmonov <shohabbos.usmonov@kpi.com>
2022-10-30 03:07:08 +0500

    first commit

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task1;
 public class PrintWordsApp {
     public static void main(String[] args) {
 
-        System.out.println("java \n is \n something");
+        System.out.println("java is something");
     }
 }
